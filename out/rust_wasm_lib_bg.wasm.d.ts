44837c94a74e0ce610e87d864bf0ab32fa883a48
hujing <hujing@eacomp.com>
2025-03-11 13:22:14 +0800

    chore(build): delete out dir

--- out/rust_wasm_lib_bg.wasm.d.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export const point_in_polygon_rayster: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number];
-export const point_in_polygon_scanline: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number];
-export const __wbindgen_export_0: WebAssembly.Table;
-export const __wbindgen_malloc: (a: number, b: number) => number;
-export const __wbindgen_free: (a: number, b: number, c: number) => void;
-export const __wbindgen_start: () => void;
